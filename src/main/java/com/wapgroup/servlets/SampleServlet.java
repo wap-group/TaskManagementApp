f02c8587430aa3503e45cdf792c4882ee9a44071
Hike <mikialebachew@gmail.com>
2019-04-21 11:55:46 +0600

    added sql-connector dependency on the pom.xml file. created DatabaseConnection.java file

--- src/main/java/com/wapgroup/servlets/SampleServlet.java
+++ src/main/java/com/wapgroup/servlets/SampleServlet.java
@@ -7,13 +7,15 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(name = "SampleServlet")
+@WebServlet("/SampleServlet")
 public class SampleServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+
 
     }
 }
